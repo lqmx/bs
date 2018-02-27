f9b468a34827400eca2fb0fa28bc23689f33f817
lqmx <lmxqmx@foxmail.com>
2018-02-27 15:30:35 +0800

    ..

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 
+//require_once(__DIR__.'/../vendor/autoload.php');
 require_once('../vendor/autoload.php');
 
 use NoahBuscher\Macaw\Macaw;
--- view/comm/js/index.js
+++ view/comm/js/index.js
@@ -13,176 +13,6 @@ $(function () {
         offAlarm:function(){
         }
     });
-    var CMD_TYPE_SYS = 1;
-    var CMD_TYPE_ALIGN = 2;
-    var CMD_TYPE_UNDEFINED = 0;
-    var cmdType = {
-        ':': CMD_TYPE_SYS,
-        '=': CMD_TYPE_ALIGN
-    };
-    var data = {};
-    var curCmd = {
-        type: CMD_TYPE_SYS,
-        content: '',
-    };
-    var params = {};
-
-    var Tool = function (data) {
-        this.data = data || {};
-        this.$me =$('.tool');
-        this.$input = $('.tool input');
-        this.$tip = $('.tool .tip');
-        this.$list = $('.tool ul');
-        this.$cmd = {};
-        this.init();
-    };
-    Tool.prototype = {
-        init: function () {
-            var self = this;
-            $.each(self.data, function (k, v) {
-                self.$list.append('<li class="clear"><span>' + v.cmd + '</span><span class="des ">'+ v.des + '</span></li>');
-            });
-            self.$input.keyup(function(event) {
-                var val = $(this).val();
-                if(val === '') {
-                    self.hide();
-                    return true;
-                }
-                if(event.key === 'Enter') {
-                    curCmd.type = getCmdType(val.substr(0, 1));
-                    curCmd.content = val.substr(1);
-//                        console.log(curCmd);
-                    self.hide();
-                    self.exec(curCmd);
-                }
-                var cmd = '';
-                if(val.length > 0) {
-                    cmd = val.substr(1);
-                }
-                self.$list.find('li').each(function (k, v) {
-                    if(cmd === '' || $(v).find('span:first-child').text().indexOf(cmd) !== -1) {
-                        $(v).show();
-                    }
-                    else {
-                        $(v).hide();
-                    }
-                });
-            });
-        },
-        hide: function () {
-            var self = this;
-            self.$input.val('');
-            self.$me.hide();
-            self.$tip.hide();
-        },
-        tip: function (tip) {
-            var self = this;
-            self.$tip.text(tip).show();
-        },
-        show: function (isShowList) {
-            var self = this;
-            if(isShowList === undefined) {
-                isShowList = true;
-            }
-            if(isShowList) {
-                self.$list.show();
-            }else {
-                self.$list.hide();
-            }
-            self.$me.show();
-            self.$input.focus();
-        },
-        undefinedCmd: function () {
-            var self = this;
-            self.show(false);
-            self.tip('undefined command');
-        },
-        exec: function (cmd) {
-            var self = this;
-            switch (cmd.type) {
-                case CMD_TYPE_SYS:
-                    switch(cmd.content) {
-                        case 'login':
-                            self.show(false);
-                            self.tip('input your name and password like: name password');
-                            self.cmd({
-                                type: CMD_TYPE_ALIGN,
-                                params: ['name', 'password'],
-                            });
-                            break;
-                        case 'exit':
-                            self.hide();
-                            break;
-                        default:
-                            self.undefinedCmd();
-                    }
-                    break;
-                case CMD_TYPE_ALIGN:
-                    var values = cmd.content.split(' ');
-                    var len = values.length;
-                    $.each(self.$cmd.params, function (k, v) {
-                        if(k < len) {
-                            eval('params.' + v + '="' + values[k] + '"');
-                        }else {
-                            eval('params.' + v + '=""');
-                        }
-                    });
-                    break;
-                default:
-                    self.undefinedCmd();
-                    break;
-            }
-        },
-        cmd: function (cmd) {
-            var self = this;
-            self.$cmd = cmd;
-            switch (cmd.type) {
-                case CMD_TYPE_ALIGN:
-                    self.$input.val("=");
-                    break;
-                case CMD_TYPE_SYS:
-                    self.$input.val(':');
-                    break;
-                default:
-                    break;
-            }
-        }
-    };
-    var toolData = [
-        {
-            cmd: 'login',
-            des: 'login the system'
-        }, {
-            cmd: 'logout',
-            des: 'logout the system'
-        }, {
-            cmd: 'exit',
-            des: 'exit the tool'
-        }
-    ];
-    initData(toolData);
-    var tool = new Tool(toolData);
-    $(document).keydown(function(event){
-        if(event.shiftKey && event.key === ':') {
-            tool.show();
-        }
-        if(event.key === 'Escape') {
-            tool.hide();
-        }
-    });
-    function getCmdType(str) {
-        if(cmdType[str]) {
-            return cmdType[str];
-        }
-        return CMD_TYPE_UNDEFINED;
-    }
-    function initData(toolData) {
-        var cmds = [];
-        $.each(toolData, function (k, v) {
-            cmds.push(v.cmd);
-        });
-        data.cmds = cmds;
-    }
 
     // bookshelf
     var Bookshelf = function (data) {
@@ -252,6 +82,13 @@ $(function () {
             self.book = self.book.concat(book);
             self.initBook();
             self.initView();
+            self.initEvt();
+        },
+        initEvt: function () {
+            $('.book').click(function () {
+                var no = $(this).find('.no').text();
+                window.location.href = "/book.html";
+            });
         },
         initBook: function () {
             var self = this;
@@ -272,8 +109,9 @@ $(function () {
                     v.h = 'l';
                 }
                 v.hs = self.hList[v.h];
-                var index = Math.abs(hash($.md5(v.name))) % (self.cList.length-1);
-                v.c = self.cList[index];
+                // var index = Math.abs(hash($.md5(v.name))) % (self.cList.length-1);
+                // v.c = self.cList[index];
+                v.c = 'c-'  + v.type;
             });
             var level = 0;
             var w = 0;
@@ -305,23 +143,26 @@ $(function () {
                     self.level += 1;
                     console.log(self.$me.find('.bookshelf-item').length);
                 }
-                self.$me.find('.bookshelf-item').eq(v.level).append('<div class="book ' + v.h + 'h-book ' + v.w + 'w-book ' + v.c + '"><div class="content"><div class="no">' + v.no + '</div><div class="s-title">' + v.name + '</div></div></div>');
+                self.$me.find('.bookshelf-item').eq(v.level).append(
+                    '<div class="book ' + v.h + 'h-book ' + v.w + 'w-book ' + v.c + '">' +
+                    '<div class="content">' +
+                    '<div class="no">' + v.no + '</div>' +
+                    '<div class="s-title">' + v.name + '</div>' +
+                    '</div>' +
+                    '</div>');
             });
-            // $('.book').each(function (k, v) {
-            //     $(v).hover(function () {
-            //         $(this).addClass('book-hover');
-            //     }, function () {
-            //         $(this).removeClass('book-hover');
-            //     });
-            // });
         }
     };
-    // var bookshelf = new Bookshelf();
-    $.post('/book/getbooks', {
-        bookNo: ''
-    }, function (data) {
-        // bookshelf.updateView(data);
-    }, "json");
+    var bookshelf = new Bookshelf();
+    // $.get('/book/getbooks', {
+    //     bookNo: ''
+    // }, function (data) {
+    //     bookshelf.updateView(data);
+    // }, "json");
+
+    $.getJSON("data/data.json", function (data) {
+        bookshelf.updateView(data);
+    });
 
     function hash(str){
         var hash = 0;
